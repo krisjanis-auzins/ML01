d3a2cf627d3e315aad74d045fdaaca8adf567076
Krisjanis Auzins <krisjanis.auzins@magebit.com>
2022-09-22 19:24:42 +0300

    corrections items.phtml, attributes.phtml; responsive styling

--- app/design/frontend/Magebit/learning/Magento_Catalog/web/js/qty-counter.js
+++ app/design/frontend/Magebit/learning/Magento_Catalog/web/js/qty-counter.js
@@ -9,23 +9,64 @@ define([
 
         initialize: function (config) {
             this.qty = ko.observable(parseInt(config.productDefaultQty));
-            this.maxQty = config.maxQty;
+            this.maxQty = parseInt(config.maxQty);
             this.dataValidate = config.dataValidate;
-            return this._super();
+            this.inputElementId = config.inputElementId;
+            this._super();
         },
 
-        increaseQty: function() {
-            const currentQty = this.qty();
-            if(currentQty < this.maxQty){
+        increaseQty: function () {
+            const currentQty = parseInt(this.qty());
+            if (currentQty < this.maxQty) {
                 this.qty(currentQty + 1);
             }
         },
 
-        decreaseQty: function() {
-            const currentQty = this.qty();
-            if(currentQty > 1){
+        decreaseQty: function () {
+            const currentQty = parseInt(this.qty());
+            if (currentQty > 1) {
                 this.qty(currentQty - 1);
             }
+        },
+
+        watchInput: function () {
+            function inputFocused () {
+                // Console logs
+                console.log("input is focused");
+                console.log(this);
+                // Console logs end
+
+                const currentQty = parseInt(this.inputElement.value);
+                const validateRangeListener = validateRange.bind(this, currentQty)
+                this.inputElement.addEventListener("focusout", validateRangeListener);
+                this.inputElement.removeEventListener('focusin', inputFocusedListener);
+            }
+
+            function validateRange (oldQty) {
+                // Console logs
+                console.log("validating input");
+                console.log(this);
+                console.log('Old qty: ' + oldQty + ", qty: " + parseInt(this.qty()));
+                // Console logs end
+
+                const currentQty = parseInt(this.inputElement.value);
+                if (currentQty < 1 || currentQty > this.maxQty) {
+                    this.qty(oldQty);
+                } else {
+                    this.qty(currentQty);
+                }
+                ;
+                this.inputElement.removeEventListener('focusout', validateRangeListener);
+            }
+
+            console.log("watching input");
+            console.log(this);
+
+            this.inputElement = document.getElementById(this.inputElementId);
+            const inputFocusedListener = inputFocused.bind(this);
+            const validateRangeListener = validateRange.bind(this);
+            this.inputElement.addEventListener('focusin', inputFocusedListener);
+            this.inputElement.addEventListener('focusout', validateRangeListener);
         },
     })
 })
