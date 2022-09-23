a771b4a502814670abf8295d7b20dee212473dd9
Krisjanis Auzins <krisjanis.auzins@magebit.com>
2022-09-23 11:21:16 +0300

    collapsible description; custom _simple.phtml files for attributes

---
+++ app/design/frontend/Magebit/learning/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'qtyCounter': 'Magento_Catalog/js/qty-counter',
+            'toggleDescription': 'Magento_Catalog/js/toggle-description'
+        }
+    }
+};
